3f9b9983801f147c2dfbcafeddc5d05dae1cdac2
Spencer Saunders <spencersaunders@pm.me>
2023-10-25 22:15:00 -0600

    working on packet_detect

--- python/monitor/packet_detect.py
+++ python/monitor/packet_detect.py
@@ -3,53 +3,100 @@ import multiprocessing as mp
 from numba import njit, jit
 from time import sleep
 
-@njit(cache=True)
-def remove_zeros(signal:np.ndarray) -> np.ndarray:
-    """Uses jit to quickly remove the block of zeros at the end of the array.
-    
-    Arguments:
-        signal (np.ndarray): The numpy array that carries the IQ packets of the signal data.
-
-    Returns:
-        np.ndarray: The numpy array with the zeros removed.
-    """
-    return np.trim_zeros(signal, trim='b')
-
-@njit(cache=True)
-def process_signal(signal:np.ndarray, threshold:float, cutoff:int) -> bool:
+@njit(cache=True, fastmath=True)
+def process_signal(signal:np.ndarray, threshold:float, cutoff:int, packet:np.ndarray) -> tuple:
     """Decides if the signal is part of a packet
     
     Arguments:
-        signal (np.ndarray): The numpy array that carries the IQ packets of the signal data.
-        threshold (float): The minimum value that a signal must exceed to be considered part of a packet.
-        cutoff (int): The number of IQ values that are allowed to be under the threshold and still be considered part of a packet.
+        signal (np.ndarray): The numpy array that carries the IQ packets
+            of the signal data.
+
+        threshold (float): The minimum value that a signal must exceed to
+            be considered part of a packet.
+
+        cutoff (int): The number of IQ values that are allowed to be under
+            the threshold and still be considered part of a packet.
 
     Returns:
-        bool: A bool indicating if the signal is part of a packet.
+        tuple (np.ndarray, bool): A tuple containing the packet and a bool
+            indicating if the packet was completed.
     """
+    signal = np.trim_zeros(signal, trim='b')
+    abs_signal:np.ndarray = np.abs(signal)
+    max_signal_value = np.max(abs_signal)
+    # If there is currently no packet detected then find if
+    # there are any signals that go above the threshold.
+    # TODO: Check if there are any values above the threshold before looping. If continuing a packet check to see if the index is larger than the cutoff point.
+    if packet == None:
+        if max_signal_value > threshold:
+            max_value_index:int = np.argmax(abs_signal)
+            signal = signal[max_value_index:]
+        else:
+            return (packet, True)
+    # Find the cutoff point of the packet in the signal
+    #! TODO: Make sure packet is not None
+    signal_cutoff:int = cutoff
+    index_above_threshold:int = None
+    for i in range(len(abs_signal)):
+        if signal_cutoff == 0 and packet == None:
+            packet = signal[:index_above_threshold+1]
+            return (packet, True)
+        if signal_cutoff == 0 and index_above_threshold == None:
+            return (packet, True)
+        elif signal_cutoff == 0 and index_above_threshold:
+            signal = signal[:index_above_threshold+1]
+            packet = np.concatenate(packet, signal)
+            return (packet, True)
+        elif signal_cutoff > 0 and abs_signal[i] > threshold:
+            index_above_threshold = i
+            signal_cutoff = cutoff
+        elif signal_cutoff > 0 and abs_signal[i] < threshold:
+            signal_cutoff -= 1
+    packet = np.concatenate(packet, signal)
+    return (packet, False)
+
+
 
 class PacketDetect:
 
-    def __init__(self, stream_q:mp.Queue, packet_counter:mp.Value, threshold:float, cutoff:int, bouncer:int, packet_q:mp.Queue, max_int:int):
-        self.stream_q = stream_q
-        self.packet_q = packet_q
-        self.packet_counter = packet_counter
-        self.threshold = threshold
-        self.cutoff = cutoff
-        self.bouncer = bouncer
-        self.max_int = max_int
+    def __init__(self, stream_q:mp.Queue, packet_counter:mp.Value, threshold:float, cutoff:int, bouncer:mp.Value, packet_q:mp.Queue, max_int:int):
+        """A class that handles detecting when a signal is part of a packet.
+        
+        Arguments:
+            stream_q (mp.Queue): A queue holding iq samples and the order in which they
+                were added.
 
-    def start(self):
+            packet_counter (mp.Value): A shared value that keeps track of which packet 
+                has been processed to maintain packet order.
+            
+            threshold (float): The value that signifies when a signal should start to
+                considered a packet.
+            
+            cutoff (int): The number of iq values that need to be below the threshold
+                for the signal to be considered ended.
+            
+            bouncer (mp.Value): A shared value that controls the order in which
+                signals are placed onto the packet_q.
+            
+            packet_q (mp.Queue): A queue holding iq samples and a bool identifying if
+                they are part of a packet.
+            
+            max_int (int): The maximum value 
+        """
+        self.stream_q:mp.Queue = stream_q
+        self.packet_q:mp.Queue = packet_q
+        self.packet_counter:mp.Value = packet_counter
+        self.threshold:float = threshold
+        self.cutoff:int = cutoff
+        self.packet:np.ndarray = None
+        # self.bouncer:mp.Value = bouncer #! may not need
+        # self.max_int:int = max_int      #! may not need
+
+    def start(self) -> None:
+        """ Decides when a signal is part of a packet. """
         while True:
-            signal, order_num = self.stream_q.get()
-            signal = remove_zeros(signal)
-            is_packet = process_signal(signal, self.threshold, self.cutoff)
-            while True:
-                # TODO: Add a watchdog here to prevent infinite loop
-                if self.bouncer == order_num:
-                    self.packet_q.put((signal,is_packet))
-                    self.packet_counter.value += 1
-                    if self.packet_counter >= self.max_int:
-                        self.packet_counter.value = 0
-                else:
-                    sleep(.0001)
+            signal = self.stream_q.get()
+            self.packet, end_of_packet = process_signal(signal, self.threshold, self.cutoff, self.packet)
+            if self.packet and end_of_packet:
+                self.packet_q.put(self.packet)
+                self.packet = None
